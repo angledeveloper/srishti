513e22e834d446c09b1bce15a5151eca60d70713
siddhant <siddhantvij001@gmail.com>
2024-05-18 23:52:23 +0530

    faviconpng

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.png' />
+      </head>
       <body className=' bg-white text-black'>{children}</body>
     </html>
   );
